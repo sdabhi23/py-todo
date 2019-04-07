30502d0dfea4e23fe7f0edfebc2a9205e4e4b8e3
Shrey <shrey.dabhi23@gmail.com>
2019-04-07 12:26:51 +0530

    remove null constraint

--- server.py
+++ server.py
@@ -30,8 +30,8 @@ class Todo(db.Model):
     title = db.Column(db.String(1024))
     message = db.Column(db.String(10240))
     is_completed = db.Column(db.Boolean, default=True, nullable=False)
-    created_on = db.Column(db.DateTime, default=datetime.utcnow(), nullable=False)
-    updated_on = db.Column(db.DateTime, default=datetime.utcnow(), nullable=False)
+    created_on = db.Column(db.DateTime, default=datetime.utcnow())
+    updated_on = db.Column(db.DateTime, default=datetime.utcnow())
 
 def id_generator(size=12, chars=string.ascii_uppercase + string.digits + string.ascii_lowercase):
     return ''.join(random.choice(chars) for _ in range(size))
